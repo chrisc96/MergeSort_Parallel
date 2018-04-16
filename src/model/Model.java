005fbb61bc57be55ada4043ebb7dfde271be2799
chrisc10196@gmail.com <chrisc10196@gmail.com>
2018-04-16 16:01:14 +1200

    Began report

--- src/model/Model.java
+++ src/model/Model.java
@@ -11,11 +11,13 @@ import java.util.Stack;
 public class Model {
     public static final double size = 900; // Size of Window (width and height)
     public static final double gravitationalConstant = 0.002;
-    public static final double lightSpeed = 10;//the smaller, the larger is the chunk of universe we simulate
-    public static final double timeFrame = 20;//the bigger, the shorter is the time of a step
-    public List<Particle> p = new ArrayList<>();
-    public volatile List<DrawableParticle> pDraw = new ArrayList<>();
+    public static final double lightSpeed = 10; //the smaller, the larger is the chunk of universe we simulate
+    public static final double timeFrame = 20; //the bigger, the shorter is the time of a step
 
+    public List<Particle> p = new ArrayList<>(); // List of particles in model
+    public volatile List<DrawableParticle> pDraw = new ArrayList<>(); //
+
+    // Model and View update. Like a tick method. Called every frame.
     public void step() {
         for (Particle p : this.p) {
             p.interact(this);
